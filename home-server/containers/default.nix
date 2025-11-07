9f80a65671384008101e6dfd202da2c039cc50bd
4JX <79868816+4JX@users.noreply.github.com>
2025-11-07 17:31:39 +0100

    feat: Prepare new file structure

---
+++ home-server/containers/default.nix
@@ -0,0 +1,31 @@
+{ ... }:
+
+{
+  imports = [
+    ./authentik
+
+    ./radarr
+    ./sonarr
+
+    ./komga
+
+    ./cloudflared
+    ./cross-seed
+    ./dnsmasq
+    ./dozzle
+    ./flaresolverr
+    ./recyclarr
+    ./gluetun
+    ./jellyfin
+    ./jellyseerr
+    ./prowlarr
+    ./qbit_manage
+    ./qbittorrent
+    ./readarr
+    ./suwayomi
+    ./swag-internal
+    ./swag
+    ./thelounge
+    ./wg-easy
+  ];
+}
