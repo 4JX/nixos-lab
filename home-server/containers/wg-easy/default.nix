63921edb7faeb10afd849363d1662ca20d441f23
4JX <79868816+4JX@users.noreply.github.com>
2025-11-10 16:15:37 +0100

    feat(wg-easy): Adapt config for v15

--- home-server/containers/wg-easy/default.nix
+++ home-server/containers/wg-easy/default.nix
@@ -1,5 +1,4 @@
 # https://github.com/wg-easy/wg-easy
-# sudo docker run --rm -it ghcr.io/wg-easy/wg-easy wgpw 'password'
 # Ports are handled in dnsmasq
 {
   lib,
@@ -36,14 +35,31 @@ in
     #   allowedUDPPorts = [ cfg.serverPort ];
     # };
 
+    # https://wg-easy.github.io/wg-easy/v15.0/faq/#cant-initialize-ip6tables-table-nat-table-does-not-exist-do-you-need-to-insmod
+    #! Use nftables:
+    # https://github.com/wg-easy/wg-easy/issues/2220
+    # https://wg-easy.github.io/wg-easy/Pre-release/examples/tutorials/podman-nft/#load-kernel-modules
+    boot.kernelModules = [
+      "wireguard"
+      "nft_masq"
+    ];
+
     # Extracted from docker-compose.nix
     virtualisation.oci-containers.containers."wg-easy" = {
-      image = "ghcr.io/wg-easy/wg-easy:15@sha256:bb8152762c36f824eb42bb2f3c5ab8ad952818fbef677d584bc69ec513b251b0";
-      environmentFiles = [
-        config.sops.secrets.wg-easy-env.path
-      ];
+      image = "ghcr.io/wg-easy/wg-easy:15.1.0@sha256:bb8152762c36f824eb42bb2f3c5ab8ad952818fbef677d584bc69ec513b251b0";
+      environment = {
+        # https://wg-easy.github.io/wg-easy/latest/advanced/config/optional-config/
+        #  - PORT=51821
+        #  - HOST=0.0.0.0
+        #  - INSECURE=false
+        "INSECURE" = "false";
+        "DISABLE_IPV6" = "true";
+      };
       volumes = [
         "/containers/config/wg-easy:/etc/wireguard:rw"
+        # - /lib/modules:/lib/modules:ro
+        # See: https://github.com/wg-easy/wg-easy/issues/1919#issuecomment-3318257458
+        # "/run/current-system/kernel-modules/lib/modules:/lib/modules:ro"
       ];
       # ports = [
       #   "${serverPortString}:51820/udp"
@@ -56,10 +72,14 @@ in
       capabilities = {
         NET_ADMIN = true;
         SYS_MODULE = true;
+        # - NET_RAW # Uncomment if using Podman
       };
       extraOptions = [
-        "--sysctl=net.ipv4.conf.all.src_valid_mark=1"
         "--sysctl=net.ipv4.ip_forward=1"
+        "--sysctl=net.ipv4.conf.all.src_valid_mark=1"
+        "--sysctl=net.ipv6.conf.all.disable_ipv6=0"
+        "--sysctl=net.ipv6.conf.all.forwarding=1"
+        "--sysctl=net.ipv6.conf.default.forwarding=1"
       ];
       networks = [
         "container:dnsmasq"
